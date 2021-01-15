2e0de096b5f3b83851b84d2bac79877fbc65cd71
yyogesh <yogeshggn1@gmail.com>
2021-01-15 15:36:48 +0530

    java if statement

--- Day 4/PrintStatement.java
+++ Day 4/PrintStatement.java
@@ -1,11 +1,10 @@
-package Day 4;
 
-public class PrintStatement {
+class PrintStatement {
     public static void main(String[] args) {
-    	
+
         System.out.println("1. println ");
         System.out.println("2. println ");
-    	
+
         System.out.print("1. print ");
         System.out.print("2. print");
     }
---
+++ Day 5/IfStatement.java
@@ -0,0 +1,9 @@
+class IfStatement {
+    public static void main(String[] args) {
+        String language = "Java";
+        // if statement
+        if (language == "Java") {
+            System.out.println("Best Programming Language");
+        }
+    }
+}
